3e3aefb61d51ff1c176f0303de86a8bc3c62d354
Balmonddd <Kentangpotato439@gmail.com>
2024-10-20 23:01:22 +0800

    memperbaharui

--- js/script.js
+++ js/script.js
@@ -14,9 +14,16 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 }
 
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+   shoppingCart.classList.toggle('active');
+   e.preventDefault();
+}
 
+//klik di luar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e){
   if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -26,6 +33,35 @@ document.addEventListener('click', function(e){
   if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove('active');
   }
+  if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 
 });
- 
+
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+})
+
+
+//klik tombol close tanda x
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+
+//klik di luar x
+const modal = document.querySelector('#item-detail-modal');
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display ='none';
+  }
+}
